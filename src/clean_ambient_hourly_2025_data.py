a9d8020045289886ed69dbfa1e3c1d4fcb15ed5a
ryanfic <ryan_Vincent_Ficocelli@Hotmail.com>
2026-07-29 14:59:04 -0700

    Fix clean_ambient_hourly_2025_data.py

--- src/clean_ambient_hourly_2025_data.py
+++ src/clean_ambient_hourly_2025_data.py
@@ -7,8 +7,8 @@ from geopy.distance import geodesic
 import h3
 
 # Input file path
-#INPUT_FILE = 'C:/Users/Ryan/Documents/Ambient_Data/monthy_hour_of_day_202501_202606.csv'
-INPUT_FILE = '../dashboard/web_dashboard/public/data/monthy_hour_of_day_202501_202606_within_250m.json'
+INPUT_FILE = 'C:/Users/Ryan/Documents/Ambient_Data/monthy_hour_of_day_202501_202606.csv'
+#INPUT_FILE = '../dashboard/web_dashboard/public/data/monthy_hour_of_day_202501_202606_within_250m.json'
 
 OUTPUT_FILE_DIRECTORY = '../dashboard/web_dashboard/public/data/'
 OUTPUT_FILE_NAME = 'monthy_hour_of_day_202501_202606'
@@ -17,7 +17,7 @@ STATIONS_INPUT_FILE = '../dashboard/web_dashboard/public/data/transit.json'
 
 # the number of rows to load (for testing purposes), set to 'None' if you want to load all data
 NUM_ROWS_TO_LOAD = None
-#NUM_ROWS_TO_LOAD = 20
+#NUM_ROWS_TO_LOAD = 10010
 
 DISPLAY_DATA = True
 
@@ -44,11 +44,11 @@ def load_hex_data(filepath):
 
     if 'NUM_ROWS_TO_LOAD' in globals():
         print("NUM_ROWS_TO_LOAD is a global variable")
-        #df = pd.read_csv(filepath, nrows=NUM_ROWS_TO_LOAD)
-        df = pd.read_json(filepath, nrows=NUM_ROWS_TO_LOAD)
+        df = pd.read_csv(filepath, nrows=NUM_ROWS_TO_LOAD)
+        #df = pd.read_json(filepath, nrows=NUM_ROWS_TO_LOAD)
     else:
-        #df = pd.read_csv(filepath)
-        df = pd.read_json(filepath)
+        df = pd.read_csv(filepath)
+        #df = pd.read_json(filepath)
 
     print(f"✓ Loaded {len(df):,} total records")
     print(f"✓ Columns: {', '.join(df.columns.tolist())}")
@@ -93,36 +93,53 @@ def filter_data_include_distance_away(hexDF: pd.DataFrame, transitDF: pd.DataFra
     filteredDF = None
     rows_list = []
     pd.set_option('display.max_columns', None)
+    rowsUpdated = 0
+
+    hashmap = {}
+
     for index, row in hexDF.iterrows():
         rowID = row['h3_l12']
         lat, lng = h3.cell_to_latlng(rowID)
         hexPos = (lat, lng)
 
         hexWithinRadius = False
-        for index, row in transitDF.iterrows():
-            features = row['features']
-            properties = features['properties']
-            station = properties['STATION']
-            geometry = features['geometry']
-            coordsAsArray = geometry['coordinates']
-            stationCoordsAsTuple = (coordsAsArray[1], coordsAsArray[0])
 
-            distanceInMeters = geodesic(hexPos, stationCoordsAsTuple).m
+        hexNotFoundInHash = hashmap.get(rowID) is None
+
+        if hexNotFoundInHash:
+            #print(rowID + " is not in the hash")
+            for stationIndex, stationRow in transitDF.iterrows():
+                features = stationRow['features']
+                properties = features['properties']
+                station = properties['STATION']
+                geometry = features['geometry']
+                coordsAsArray = geometry['coordinates']
+                stationCoordsAsTuple = (coordsAsArray[1], coordsAsArray[0])
+
+                distanceInMeters = geodesic(hexPos, stationCoordsAsTuple).m
 
 
-            #print("distance: " + str(distanceInMeters) + " (" + station + ")")
-            hexWithinRadius = (distanceInMeters <= maxDistance)
-            if hexWithinRadius:
-                #print(station + " -> " + str(coordsAsTuple))
-                print("WITHIN RADIUS OF " + station)
-                break
-
-        if not hexWithinRadius:
-            print("would ignore this hex")
+                #print("distance: " + str(distanceInMeters) + " (" + station + ")")
+                hexWithinRadius = (distanceInMeters <= maxDistance)
+                if hexWithinRadius:
+                    #print(station + " -> " + str(coordsAsTuple))
+                    #print("WITHIN RADIUS OF " + station)
+                    hashmap[rowID] = True
+                    break
+            if not hexWithinRadius:
+                hashmap[rowID] = False
         else:
+            #print(rowID + " IS IN THE HASH")
+            hexWithinRadius = hashmap.get(rowID) is True
+
+        if hexWithinRadius:
             rows_list.append(row)
 
-        print(rowID + " [" + str(lat) + ',' + str(lng) + "]")
+        if (rowsUpdated%1000 == 0):
+            print("Updated " + str(rowsUpdated) + " rows")
+
+        rowsUpdated += 1
+        #print(rowID + " [" + str(lat) + ',' + str(lng) + "]")
 
     filteredDF = pd.DataFrame(rows_list)
     return filteredDF
@@ -147,7 +164,7 @@ def main():
     hexDF = load_hex_data(INPUT_FILE)
     transitDF = load_transit_data(STATIONS_INPUT_FILE)
 
-    maxDistance = 150
+    maxDistance = 250
 
     if DISPLAY_DATA:
         print("")
@@ -158,7 +175,7 @@ def main():
         print("")
         cleanedDF = filter_data_include_distance_away(hexDF, transitDF, maxDistance)
         print__data(cleanedDF)
-        save_data(hexDF, maxDistance)
+        save_data(cleanedDF, maxDistance)
 
 if __name__ == "__main__":
     main()
